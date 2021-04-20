75af03af5e7b16a0d690972447dc611605d67e70
hclark45 <Rap.GODD@icloud.com>
2021-04-20 12:32:28 -0400

    added automatic keys

--- Key.java
+++ Key.java
@@ -2,17 +2,42 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, and Greenfoot)
 
 public class Key extends Actor
 {
+    private boolean isDown;
+    private String key;
+    private String sound;
     /**
      * Create a new key.
      */
-    public Key()
+    public Key(String keyName, String soundFile)
     {
+        key = keyName;
+        sound = soundFile;
     }
-
     /**
      * Do the action for this key.
      */
     public void act()
     {
+        if ( !isDown && Greenfoot.isKeyDown (key))        
+           
+        {
+            setImage ("white-key-down.png");
+            play();
+            isDown = true;        
+        }
+        if ( isDown && !Greenfoot.isKeyDown (key))       
+                    
+        {   
+
+            setImage ("white-key.png");
+            isDown = false;        
+        }
+    }
+    /**
+     * Play the note of this key.
+     */
+    public void play()
+    {
+        Greenfoot.playSound(sound);
     }
 }
